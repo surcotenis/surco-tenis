54c31349b58b1ce87cc54ce7131becf3b0363793
joseyzambranov <joseyzambranovpe@gmail.com>
2023-05-25 00:00:58 -0500

    added edit cliente

--- routes/cliente.js
+++ routes/cliente.js
@@ -23,5 +23,50 @@ router.get('/perfil/:codCliente',verifyTokenAndAuthorization, (req, res) => {
       }
     });
   });
+
+  router.put('/perfil/:codCliente', verifyTokenAndAuthorization, (req, res) => {
+    const codCliente = req.params.codCliente;
+    const updatedFields = req.body;
+    
+    // Verificar si el perfil de usuario existe antes de realizar la actualización
+    const checkProfileQuery = 'SELECT * FROM cliente WHERE codCliente = ?';
+    db.query(checkProfileQuery, [codCliente], (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Error en el servidor' });
+      }
+      
+      if (results.length === 0) {
+        return res.status(404).json({ error: 'Perfil de usuario no encontrado' });
+      }
+      
+      // Construir la consulta de actualización y los valores dinámicamente
+      let updateProfileQuery = 'UPDATE cliente SET ';
+      const updateValues = [];
+      
+      for (const key in updatedFields) {
+        updateProfileQuery += `${key} = ?, `;
+        updateValues.push(updatedFields[key]);
+      }
+      
+      updateProfileQuery = updateProfileQuery.slice(0, -2); // Eliminar la última coma y espacio
+      
+      updateProfileQuery += ' WHERE codCliente = ?';
+      updateValues.push(codCliente);
+      
+      // Realizar la actualización del perfil de usuario
+      db.query(updateProfileQuery, updateValues, (error, results) => {
+        if (error) {
+          console.error(error);
+          return res.status(500).json({ error: 'Error en el servidor' });
+        }
+        
+        // Si la actualización se realiza correctamente, puedes enviar una respuesta exitosa
+        return res.json({ message: 'Perfil de usuario actualizado correctamente' });
+      });
+    });
+  });
+  
+  
   
   module.exports = router;
