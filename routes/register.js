7c1c028e3b26dd588d39604240655da441aa6bdc
jose zambrano <jose.zambrano@sinapsiscorp.com>
2023-05-30 12:19:33 -0500

    restorage encrypt

--- routes/register.js
+++ routes/register.js
@@ -56,7 +56,7 @@ router.post('/', [
         nombres,
         email,
         telefono,
-        password: await bcrypt.hash(password, 0),
+        password: await bcrypt.hash(password, 10),
         estado: 'ACTIVO',
         tipo: 'CLIENTE',
         creacion: new Date().toISOString(),
