3ef5a26bcf1213ea06b13a59291328f38310a319
joseyzambranov <joseyzambranovpe@gmail.com>
2023-06-01 22:47:55 -0500

    restorage master

--- core/db_config.js
+++ core/db_config.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2')
+const mysql = require('mysql')
 
 const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.DB_PORT,
+  //port: process.env.DB_PORT,
   });
 
   module.exports = connection
